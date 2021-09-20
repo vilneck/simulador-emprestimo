17e714bff7d7cb9bab2146a11b227f8caff9de6f
Igor Oliveira Vilneck <igvilneck@gmail.com>
2021-09-20 15:52:18 -0300

    Ajustes de responsividade

--- resources/views/layout/default.blade.php
+++ resources/views/layout/default.blade.php
@@ -1,43 +1,42 @@
 <!DOCTYPE html>
 <html dir="ltr" lang="en-US">
+
 <head>
 
-	<meta http-equiv="content-type" content="text/html; charset=utf-8" />
-	<meta name="author" content="SemiColonWeb" />
+    <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+    <meta name="author" content="SemiColonWeb" />
 
-	<!-- Stylesheets
-	============================================= -->
-	<link href="https://fonts.googleapis.com/css?family=Lato:300,400,400i,700|Poppins:300,400,500,600,700|PT+Serif:400,400i&display=swap" rel="stylesheet" type="text/css" />
-	<link rel="stylesheet" href="{{ asset('css/bootstrap.css') }}" type="text/css" />
-	<link rel="stylesheet" href="{{ asset('style.css') }}"  type="text/css" />
-	<link rel="stylesheet" href="{{ asset('css/dark.css') }}" type="text/css" />
-	<link rel="stylesheet" href="{{ asset('css/font-icons.css') }}" type="text/css" />
-	<link rel="stylesheet" href="{{ asset('css/animate.css') }}" type="text/css" />
-	<link rel="stylesheet" href="{{ asset('css/magnific-popup.css') }}" type="text/css" />
+    <!-- Stylesheets
+ ============================================= -->
+    <link
+        href="https://fonts.googleapis.com/css?family=Lato:300,400,400i,700|Poppins:300,400,500,600,700|PT+Serif:400,400i&display=swap"
+        rel="stylesheet" type="text/css" />
+    <link rel="stylesheet" href="{{ asset('css/bootstrap.css') }}" type="text/css" />
+    <link rel="stylesheet" href="{{ asset('style.css') }}" type="text/css" />
+    <link rel="stylesheet" href="{{ asset('css/dark.css') }}" type="text/css" />
+    <link rel="stylesheet" href="{{ asset('css/font-icons.css') }}" type="text/css" />
+    <link rel="stylesheet" href="{{ asset('css/animate.css') }}" type="text/css" />
+    <link rel="stylesheet" href="{{ asset('css/magnific-popup.css') }}" type="text/css" />
 
-	<link rel="stylesheet" href="{!! asset('css/custom.css') !!}" type="text/css" />
+    <link rel="stylesheet" href="{!! asset('css/custom.css') !!}" type="text/css" />
 
-	<meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
 
-	<!-- Document Title
-	============================================= -->
-	<title>Simulador de empréstimo</title>
+    <!-- Document Title
+ ============================================= -->
+    <title>Simulador de empréstimo</title>
 
 </head>
 
-<body class="stretched page-transition" data-loader="2" data-animation-in="fadeIn" data-speed-in="1500" data-animation-out="fadeOut" data-speed-out="800">
+<body class="stretched page-transition" data-loader="2" data-animation-in="fadeIn" data-speed-in="1500"
+    data-animation-out="fadeOut" data-speed-out="800">
 
-	<!-- Document Wrapper
-	============================================= -->
-	<div id="wrapper" class="clearfix">
-
-		<!-- Content
-		============================================= -->
-		<section id="content">
-			<div class="content-wrap">
-
-				<div class="container center clearfix">
-
+    <!-- Document Wrapper
+ ============================================= -->
+    <div id="wrapper" class="clearfix">
+        <section id="content">
+            <div class="content-wrap">
+                <div class="container center clearfix">
                     <div class="card">
                         <div class="card-header pt-5">
                             <div class="heading-block center">
@@ -49,24 +48,26 @@
                             @yield('content')
                         </div>
                     </div>
-				</div>
-			</div>
-		</section><!-- #content end -->
+                </div>
+            </div>
+        </section>
+    </div>
 
-	</div><!-- #wrapper end -->
+    <!-- Go To Top
+ ============================================= -->
+    <div id="gotoTop" class="icon-angle-up"></div>
 
-	<!-- Go To Top
-	============================================= -->
-	<div id="gotoTop" class="icon-angle-up"></div>
-
-	<!-- JavaScripts
-	============================================= -->
-	<script src="{!! asset('js/jquery.js') !!}"></script>
-	<script src="{!! asset('js/plugins.min.js') !!}"></script>
-	<script src="{!! asset('js/functions.js') !!}"></script>
+    <!-- JavaScripts
+ ============================================= -->
+    <script src="{!! asset('js/jquery.js') !!}"></script>
+    <script src="{!! asset('js/plugins.min.js') !!}"></script>
+    <script src="{!! asset('js/functions.js') !!}"></script>
     <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
     <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-maskmoney/3.0.2/jquery.maskMoney.min.js" integrity="sha512-Rdk63VC+1UYzGSgd3u2iadi0joUrcwX0IWp2rTh6KXFoAmgOjRS99Vynz1lJPT8dLjvo6JZOqpAHJyfCEZ5KoA==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-maskmoney/3.0.2/jquery.maskMoney.min.js"
+        integrity="sha512-Rdk63VC+1UYzGSgd3u2iadi0joUrcwX0IWp2rTh6KXFoAmgOjRS99Vynz1lJPT8dLjvo6JZOqpAHJyfCEZ5KoA=="
+        crossorigin="anonymous" referrerpolicy="no-referrer"></script>
     @yield('scripts')
 </body>
+
 </html>
--- resources/views/simulator/index.blade.php
+++ resources/views/simulator/index.blade.php
@@ -1,12 +1,12 @@
 @extends('layout.default')
 
 @section('content')
-    <div class="col-12 ">
+    <div class="col-12">
         <form method="POST" enctype="multipart/form-data">
             @csrf
 
             <div class="row justify-content-center">
-                <div class="col-6">
+                <div class="col-md-8 col-sm-12">
                     @if ($errors->any())
                         <div class="alert alert-danger">
                             @foreach ($errors->all() as $error)
@@ -17,7 +17,7 @@
                 </div>
             </div>
             <div class="row justify-content-center">
-                <div class="col-8">
+                <div class="col-md-8 col-sm-12">
                     <label for="car">Selecione o veículo<small class="text-danger">*</small></label>
                     <select onchange="onChangeCar()" id="car" name="car" class="form-select valid">
                         <option value="" disabled="" selected="">Selecione um veículo</option>
@@ -30,7 +30,7 @@
             </div>
             <span class="car-details hidden">
                 <div class="row mt-3 justify-content-center">
-                    <div class="col-8">
+                    <div class="col-md-8 col-sm-12">
                         <label>Detalhes do veículo:</label>
                         <table class="table table-condensed">
                             <tr>
@@ -53,11 +53,11 @@
                     </div>
                 </div>
                 <div class="row justify-content-center align-items-end">
-                    <div class="col-5">
+                    <div class="col-md-5 col-sm-12">
                         <label for="entry_value">Informe o valor de entrada:</label>
                         <input type="text" id="entry_value" name="entry_value" value="{{isset($data)?$data->entry_value:''}}" class="form-control" />
                     </div>
-                    <div class="col-3">
+                    <div class="col-md-3 col-sm-12">
                         <button type="submit" style="margin-bottom:0px;width:100%;"
                             class="button button-rounded button-reveal button-small button-teal"><i
                                 class="icon-dollar"></i><span>Simular</span></a>
@@ -65,7 +65,7 @@
                 </div>
                 @if (isset($html))
                 <div class="row justify-content-center align-items-end mt-3 result">
-                    <div class="col-8 alert alert-info">
+                    <div class="col-md-8 col-sm-12 alert alert-info">
                         <label>Valores simulados para você:</label>
                         <br>
                         {!! $html !!}
